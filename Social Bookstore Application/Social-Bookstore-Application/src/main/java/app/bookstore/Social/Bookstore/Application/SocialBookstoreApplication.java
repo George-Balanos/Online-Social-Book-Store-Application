038e1b4f4e6440cdd552fb8f21f8868da008e9f5
Georgios-Mpalanos <mpalanosg2002@gmail.com>
2024-03-19 17:37:20 +0200

    Update #1

---
+++ Social Bookstore Application/Social-Bookstore-Application/src/main/java/app/bookstore/Social/Bookstore/Application/SocialBookstoreApplication.java
@@ -0,0 +1,13 @@
+package app.bookstore.Social.Bookstore.Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialBookstoreApplication.class, args);
+	}
+
+}

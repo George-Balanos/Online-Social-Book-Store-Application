2036891367299596b4bcedab6cce64332ae00ade
Georgios-Mpalanos <mpalanosg2002@gmail.com>
2024-03-20 21:10:21 +0200

    Update

---
+++ socialbookstore/socialbookstore/src/main/java/app/bookstore/socialbookstore/SocialbookstoreApplication.java
@@ -0,0 +1,13 @@
+package app.bookstore.socialbookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialbookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialbookstoreApplication.class, args);
+	}
+
+}
---
+++ socialbookstore/socialbookstore/src/test/java/app/bookstore/socialbookstore/SocialbookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package app.bookstore.socialbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialbookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
